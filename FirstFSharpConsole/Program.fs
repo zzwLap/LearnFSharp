7f895afa445b3f0caaaa9e6cd5858598d353e110
zhangzhiwei <806804985@qq.com>
2025-07-12 09:05:42 +0800

    语法学习

--- FirstFSharpConsole/Program.fs
+++ FirstFSharpConsole/Program.fs
@@ -1,8 +1,20 @@
-﻿
-let [<Literal>] LiteralStr = "hello world"
+﻿let rec fib n =
+    match n with
+    | 0 | 1 -> n
+    | n -> fib (n-1) + fib (n-2)
 
-[<Literal>] 
-let LiteralStr2 = "hello world"
+//implicit rec keyword
+type MyClass() =
+    member this.Fib(n) =
+        match n with
+        | 0 | 1 -> n
+        | n -> this.Fib(n-1) + this.Fib(n-2)
 
-printfn "%s" LiteralStr
-printfn "%s" LiteralStr2
+//Mutually Recursive Functions
+let rec Even x = if x = 0 then true else Odd(x - 1)
+and Odd x = if x = 0 then false else Even(x - 1)
+
+//Recursive values
+let rec nameDoubles = nameof nameDoubles + nameof nameDoubles
+
+
